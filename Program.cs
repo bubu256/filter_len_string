75e92ee5f2507b4c1ad1fee2aed69d0f6291da04
rettopekim <bulostm@gmail.com>
2022-11-09 16:49:30 +0300

    added FilterLenArray function changed block_diagram.jpg added comments and small fixes in main code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿string[] InputStringArray(int len)
+﻿// в консоли запрос len строк от пользователя
+string[] InputStringArray(int len)
 {
     string[] result = new string[len];
     for (int i = 0; i < len; i++)
@@ -8,6 +9,7 @@
     return result;
 }
 
+// вывод массива строк в консоль
 void PrintStringArray(string[] strArray)
 {
     for (int i = 0; i < strArray.Length; i++)
@@ -16,10 +18,34 @@ void PrintStringArray(string[] strArray)
     }
 }
 
+// возвращает отфильтрованный массив строк, 
+// с строками длиной меньше или равно maxLen
+string[] FilterLenArray(string[] strArray, int maxLen = 3)
+{
+    int count = 0;
+    for(int i=0; i<strArray.Length; i++)
+        if (strArray[i].Length <= maxLen) count++;
+    
+    string[] result = new string[count];
+    count = 0;
+
+    for (int i=0; i<strArray.Length; i++)
+    {
+        if (strArray[i].Length<=maxLen)
+        {
+            result[count] = strArray[i];
+            count++;
+        }
+    }
+    return result;
+}
+
+
 Console.Write("Введите количество элементов массива: ");
 int num = Int16.Parse(Console.ReadLine());
+Console.WriteLine("Вводите строки через Enter:");
 
 string[] inp_array = InputStringArray(num);
 
 Console.WriteLine("Вывод: ");
-PrintStringArray(inp_array);
+PrintStringArray(FilterLenArray(inp_array));
