c4172040444a452d17366b7e3353edffb8ea8f11
rettopekim <bulostm@gmail.com>
2022-11-09 16:28:21 +0300

    added input/output functions

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿string[] InputStringArray(int len)
+{
+    string[] result = new string[len];
+    for (int i = 0; i < len; i++)
+    {
+        result[i] = Console.ReadLine();
+    }
+    return result;
+}
+
+void PrintStringArray(string[] strArray)
+{
+    for (int i = 0; i < strArray.Length; i++)
+    {
+        Console.WriteLine(strArray[i]);
+    }
+}
+
+Console.Write("Введите количество элементов массива: ");
+int num = Int16.Parse(Console.ReadLine());
+
+string[] inp_array = InputStringArray(num);
+
+Console.WriteLine("Вывод: ");
+PrintStringArray(inp_array);
